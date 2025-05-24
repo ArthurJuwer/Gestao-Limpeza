8dc37cb1e2ebbe6bcfc3d0599753db6dae9372cd
ArthurJuwer <arthur.juwer99@gmail.com>
2025-05-24 13:10:06 -0300

    novas atualizações principalmente em dark: e avisos

---
+++ js/avisos.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dadosUsuario = JSON.parse(localStorage.getItem('usuario'));
+
+
+    if(dadosUsuario.corsistema === "escuro"){
+        document.documentElement.classList.add('dark');
+    } 
+
+    const menuName = document.querySelector("#menu-name");
+    const menuFoto = document.querySelector("#menu-foto");
+
+    if (menuName) menuName.textContent = dadosUsuario.nome;
+    if (menuFoto) menuFoto.src = dadosUsuario.foto;
+
+    function contarTotalTarefas(equipe) {
+        return equipe.tarefas.reduce((total, projeto) => total + projeto.tarefas.length, 0);
+    }
+
+    dadosUsuario.equipes.forEach((equipe, index) => {
+        const numEquipe = index + 1; 
+
+        const totalProjetosEl = document.querySelector(`#total-projetos-0${numEquipe}`);
+        if (totalProjetosEl) {
+            totalProjetosEl.textContent = equipe.numprojetos.num;
+        }
+
+        const totalTarefasEl = document.querySelector(`#total-tarefas-0${numEquipe}`);
+        if (totalTarefasEl) {
+            const totalTarefas = contarTotalTarefas(equipe);
+            totalTarefasEl.textContent = totalTarefas;
+        }
+
+        const totalCargoEl = document.querySelector(`#total-cargo-0${numEquipe}`);
+        if (totalCargoEl) {
+            const cargoSimplificado = equipe.cargo.split(' ')[0]; // Pega só a primeira palavra
+            totalCargoEl.textContent = cargoSimplificado
+        }
+    });
+});
+
+
--- js/equipe.js
+++ js/equipe.js
@@ -1,3 +1,34 @@
+let dadosUsuario = JSON.parse(localStorage.getItem('usuario'));
+
+if (dadosUsuario.equipeatual) {
+    const equipeAtual = dadosUsuario.equipes.find(equipe => equipe.nome === dadosUsuario.equipeatual);
+
+    if (equipeAtual) {
+        console.log("Cargo na equipe atual:", equipeAtual.cargo);
+
+        // Exemplo: Atualizar algum elemento da página com o cargo
+        const cargoElemento = document.querySelector("#cargo");
+        if (cargoElemento) {
+            cargoElemento.textContent = `${equipeAtual.cargo}`;
+        }
+    } else {
+        console.log("Equipe atual não encontrada.");
+    }
+} else {
+    console.log("Nenhuma equipe atual selecionada.");
+}
+const menuName = document.querySelector("#menu-name");
+const menuFoto = document.querySelector("#menu-foto");
+
+
+  if (menuName) {
+    menuName.textContent = dadosUsuario.nome;
+  }
+
+  if (menuFoto) {
+    menuFoto.src = dadosUsuario.foto;
+  }
+
 if(dadosUsuario.corsistema === "escuro"){
     document.documentElement.classList.add('dark');
 } 
